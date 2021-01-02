6a776e54e8ee83f6606de6e2b704baf3ffbd0259
shunpei.kanbayashi <gkhandball1@gmail.com>
2021-01-02 23:58:38 +0900

    add user create.php

---
+++ main/user/create.php
@@ -0,0 +1,20 @@
+<?php
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $authority = $_POST["authority"];
+  include("../../connect/connect.php");
+  $sql = "insert into user (name,email,password,authority) value ('".$name."','".$email."','".$password."',".$authority.")";
+  var_dump($sql);
+  
+  $result = mysqli_query($db,$sql);
+  mysqli_close($db);
+
+  if ($result) {
+    header('Location: ../index.php');
+    exit();
+  } else {
+    header('Location: new.php');
+    exit(); 
+  }
+?>
